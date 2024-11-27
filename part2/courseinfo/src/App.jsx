2570b9c050fb501cded8850517545ab684f54ff5
Nayyer Ahmad <nayyer.amd@gmail.com>
2024-11-27 12:24:22 +0200

    refactor: move Course component to a separate module

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,33 +1,4 @@
-const Course = ({ course }) => {
-  const Header = ({ courseName }) => <h1>{courseName}</h1>;
-
-  const Part = ({ name, exercises }) => (
-    <p>
-      {name} {exercises}
-    </p>
-  );
-
-  const Content = ({ parts }) => (
-    <div>
-      {parts.map((part) => (
-        <Part key={part.id} name={part.name} exercises={part.exercises} />
-      ))}
-    </div>
-  );
-
-  const Total = ({ parts }) => {
-    const total = parts.reduce((sum, part) => sum + part.exercises, 0);
-    return <p>Total exercises: {total}</p>;
-  };
-
-  return (
-    <div>
-      <Header courseName={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  );
-};
+import Course from './Course';
 
 const App = () => {
   const courses = [
