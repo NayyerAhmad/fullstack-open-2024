86bc73863dfc6603c96943907d8a474e8f251943
Nayyer Ahmad <nayyer.amd@gmail.com>
2024-11-27 14:49:21 +0200

    feat: prevent adding duplicate names and show alert for duplicates

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -10,7 +10,11 @@ const App = () => {
 
   const addPerson = (event) => {
     event.preventDefault()
-    setPersons(persons.concat({ name: newName }))
+    if (persons.some(person => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`)
+    } else {
+      setPersons(persons.concat({ name: newName }))
+    }
     setNewName('')
   }
 
