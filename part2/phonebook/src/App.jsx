3f1fba8cf03844b0f2fd5b55be676ba913cf47a9
Nayyer Ahmad <nayyer.amd@gmail.com>
2024-11-27 16:59:35 +0200

    feat: Save phonebook entries to the backend using POST request

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,6 +3,7 @@ import axios from 'axios'
 import Filter from './Filter'
 import PersonForm from './PersonForm'
 import Persons from './Persons'
+import personService from './services/persons' // Import the service
 
 const App = () => {
   const [persons, setPersons] = useState([])
@@ -11,13 +12,11 @@ const App = () => {
   const [search, setSearch] = useState('')
 
   useEffect(() => {
-    axios.get('http://localhost:3001/persons')
-      .then(response => {
-        setPersons(response.data)
-      })
-      .catch(error => {
-        console.error('There was an error fetching the persons:', error)
-      })
+    personService.getAll().then(response => {
+      setPersons(response)
+    }).catch(error => {
+      console.error('There was an error fetching the persons:', error)
+    })
   }, [])
 
   const handleNameChange = (event) => {
@@ -37,8 +36,12 @@ const App = () => {
     if (persons.some(person => person.name === newName)) {
       alert(`${newName} is already added to phonebook`)
     } else {
-      const newPerson = { name: newName, number: newNumber, id: persons.length + 1 }
-      setPersons(persons.concat(newPerson))
+      const newPerson = { name: newName, number: newNumber }
+      personService.create(newPerson).then(returnedPerson => {
+        setPersons(persons.concat(returnedPerson)) // Add to state after successful creation
+      }).catch(error => {
+        console.error('Error adding person:', error)
+      })
     }
     setNewName('')
     setNewNumber('')
@@ -51,21 +54,16 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-
       <Filter value={search} onChange={handleSearchChange} />
-
       <h3>Add a new</h3>
-
-      <PersonForm 
-        onSubmit={addPerson} 
-        newName={newName} 
-        onNameChange={handleNameChange} 
-        newNumber={newNumber} 
-        onNumberChange={handleNumberChange} 
+      <PersonForm
+        onSubmit={addPerson}
+        newName={newName}
+        onNameChange={handleNameChange}
+        newNumber={newNumber}
+        onNumberChange={handleNumberChange}
       />
-
       <h3>Numbers</h3>
-
       <Persons persons={filteredPersons} />
     </div>
   )
