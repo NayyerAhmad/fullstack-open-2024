c1682793e7994718d576ef22dcecf88f8f9bc68e
Nayyer Ahmad <nayyer.amd@gmail.com>
2025-09-13 20:18:20 +0300

    backend with hardcoded entries

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,70 +1,18 @@
-const express = require('express');
-const morgan = require('morgan');
-const app = express();
+const express = require('express')
+const app = express()
 
-app.use(express.json());
-app.use(morgan('tiny'));
-
-morgan.token('body', (req) => JSON.stringify(req.body)); 
-
-app.use(morgan(':method :url :status :response-time ms - :body'));
-
-const phonebook = [
+const persons = [
   { id: "1", name: "Arto Hellas", number: "040-123456" },
   { id: "2", name: "Ada Lovelace", number: "39-44-5323523" },
   { id: "3", name: "Dan Abramov", number: "12-43-234345" },
   { id: "4", name: "Mary Poppendieck", number: "39-23-6423122" }
-];
+]
 
 app.get('/api/persons', (req, res) => {
-  res.json(phonebook);
-});
+  res.json(persons)
+})
 
-app.get('/info', (req, res) => {
-  const currentTime = new Date();
-  res.send(`
-    <p>Phonebook has info for ${phonebook.length} people</p>
-    <p>${currentTime}</p>
-  `);
-});
-
-app.get('/api/persons/:id', (req, res) => {
-  const person = phonebook.find(p => p.id === req.params.id);
-  if (person) {
-    res.json(person);
-  } else {
-    res.status(404).send({ error: 'Person not found' });
-  }
-});
-
-app.delete('/api/persons/:id', (req, res) => {
-  const id = req.params.id;
-  const index = phonebook.findIndex(p => p.id === id);
-  if (index !== -1) {
-    phonebook.splice(index, 1);
-    res.status(204).end();
-  } else {
-    res.status(404).send({ error: 'Person not found' });
-  }
-});
-
-app.post('/api/persons', (req, res) => {
-    const { name, number } = req.body;
-    if (!name || !number) {
-      return res.status(400).send({ error: 'name or number is missing' });
-    }
-  
-    const existingPerson = phonebook.find(p => p.name === name);
-    if (existingPerson) {
-      return res.status(400).send({ error: 'name must be unique' });
-    }
-  
-    const newId = Math.random().toString(36).substr(2, 9);
-    const newPerson = { id: newId, name, number };
-    phonebook.push(newPerson); 
-    res.status(201).json(newPerson);
-});
-
-app.listen(3001, () => {
-  console.log('Server running on http://localhost:3001');
-});
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
